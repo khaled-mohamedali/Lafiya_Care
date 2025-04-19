8d50430fae47a25f75d3d16dfa81890915529925
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-04-18 23:22:27 -0400

    Added a listview of pharmacies

---
+++ app/src/main/java/net/devcircuit/lafiyacare/Pharmacy.java
@@ -0,0 +1,116 @@
+package net.devcircuit.lafiyacare;
+import java.util.List;
+
+public class Pharmacy {
+
+
+
+        private String name;
+        private String address;
+        private String phone;
+        private String openHours;
+        private boolean isOpenNow;
+        private double distanceKm;
+        private double rating;
+        private String imageUrl;
+        private boolean isEmergency;
+        public Pharmacy(String name){
+            this.name = name;
+        }
+
+        // Constructor
+        public Pharmacy(String name, String address, String phone, String openHours, boolean isOpenNow,
+                        double distanceKm, double rating,
+                        String imageUrl, boolean isEmergency) {
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+            this.openHours = openHours;
+            this.isOpenNow = isOpenNow;
+            this.distanceKm = distanceKm;
+            this.rating = rating;
+
+            this.imageUrl = imageUrl;
+            this.isEmergency = isEmergency;
+        }
+
+        // Getters and Setters
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public String getOpenHours() {
+            return openHours;
+        }
+
+        public void setOpenHours(String openHours) {
+            this.openHours = openHours;
+        }
+
+        public boolean isOpenNow() {
+            return isOpenNow;
+        }
+
+        public void setOpenNow(boolean openNow) {
+            isOpenNow = openNow;
+        }
+
+        public double getDistanceKm() {
+            return distanceKm;
+        }
+
+        public void setDistanceKm(double distanceKm) {
+            this.distanceKm = distanceKm;
+        }
+
+        public double getRating() {
+            return rating;
+        }
+
+        public void setRating(double rating) {
+            this.rating = rating;
+        }
+
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public boolean isEmergency() {
+            return isEmergency;
+        }
+
+        public void setEmergency(boolean emergency) {
+            isEmergency = emergency;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+}
+
+
--- app/src/main/java/net/devcircuit/lafiyacare/PharmacyListFragment.java
+++ app/src/main/java/net/devcircuit/lafiyacare/PharmacyListFragment.java
@@ -2,20 +2,51 @@ package net.devcircuit.lafiyacare;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PharmacyListFragment extends Fragment {
 
 
+    ArrayList<Pharmacy> pharmacyList;
+
+    ArrayAdapter<Pharmacy> adapter;
+
+    ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        pharmacyList = new ArrayList<>();
+        // Add Pharmacy instances to the list
+        pharmacyList.add(new Pharmacy("Pharmacie Ridwane"));
+        pharmacyList.add(new Pharmacy("Pharmacie Recasement"));
+        pharmacyList.add(new Pharmacy("Pharmacie Sixième"));
+        pharmacyList.add(new Pharmacy("Pharmacie des Arènes"));
+        pharmacyList.add(new Pharmacy("Pharmacie de l’Avenir"));
+        pharmacyList.add(new Pharmacy("Pharmacie Cité BECEAO"));
+        pharmacyList.add(new Pharmacy("Pharmacie EL NASR"));
+        pharmacyList.add(new Pharmacy("Pharmacie Sira"));
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        listView = view.findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, pharmacyList);
+        listView.setAdapter(adapter);
     }
 
     @Override
